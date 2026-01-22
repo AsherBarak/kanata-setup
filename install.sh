bcacc6c0d197bd579d7f2b17d88feecca1e1e9ad
Asher Barak <asher.barak@gmail.com>
2026-01-22 13:21:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,8 @@ if ! command -v brew >/dev/null 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-echo "[+] Installing Kanata + tray..."
+echo "[+] Installing Kanata..."
 brew install kanata
-brew install --cask kanata-tray || true
 
 echo "[+] Cloning or updating repo..."
 DEST="$HOME/.kanata-setup"
